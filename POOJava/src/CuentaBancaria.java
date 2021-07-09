03a16c06bd5a11b84fb679cd771d997dfb7c291d
proivited <miguel.alexis.gomez.a@gmail.com>
2021-07-09 17:58:22 -0500

    Cuenta bancaria y creacion de objetos.

---
+++ POOJava/src/CuentaBancaria.java
@@ -0,0 +1,39 @@
+
+public class CuentaBancaria {
+	
+	//atributos
+	String beneficiario = "";
+	float saldo = 0;
+	long cuenta = 0;
+	String producto = "";
+	String direccion = "";
+	String RFC = "";
+	String institucion = "";
+	
+	CuentaBancaria(String beneficiario, float saldo){
+		this.beneficiario = beneficiario;
+		this.saldo = saldo;
+	}
+	
+	//metodo de la clase
+	public void depositos(float monto){
+		this.saldo = this.saldo + monto;
+	}
+	
+	public void retiros(float monto) {
+		if(monto <= this.saldo && monto > 0) {
+			this.saldo = this.saldo - monto;	
+		}
+			
+		
+	}
+	
+	public float consultarSaldo() {
+		return this.saldo;
+	}
+	
+	public String estadoDeCuenta() {
+		return "";
+	}
+
+}
---
+++ POOJava/src/POOJava.java
@@ -0,0 +1,96 @@
+import java.util.Scanner;
+
+public class POOJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		CuentaBancaria cuenta_personal = new CuentaBancaria("Miguel Alexis",
+				675);
+		CuentaBancaria cuenta_secundaria = new CuentaBancaria("Sebastian elkks",
+				9875);
+		
+		/**
+		 * menu cajero
+		 * 1. retiro
+		 * 2. deposito
+		 * 3.imprimir saldo
+		 * 4. estado de uenta
+		 * 5 Agregar beneficiario
+		 * 6 salir
+		 */
+		
+		
+		int opcion = 0;
+		
+		do {
+			Scanner leer = new Scanner(System.in);
+			
+			System.out.println("Menu cuenta banaria");
+			System.out.println("Ingrese la opcion deseada");
+			System.out.println("1. Retiro");
+			System.out.println("2. Deposito");
+			System.out.println("3. Imprimir saldo");
+			System.out.println("4. Estado de cuenta");
+			System.out.println("5. Cambiar usuario");
+			System.out.println("6. Salir");
+			
+			opcion = leer.nextInt();
+			
+			switch(opcion) {
+			case 1:
+				System.out.println("Ingrese la cantidad que desea retirar");
+				float monto = leer.nextFloat();
+				cuenta_personal.retiros(monto);
+				break;
+			case 2:
+				System.out.println("Ingrese la cantidad que desea depositar");
+				float deposito = leer.nextFloat();
+				cuenta_personal.depositos(deposito);
+				break;
+			case 3:
+				System.out.println("Su saldo es: " + 
+				cuenta_personal.consultarSaldo());
+				break;
+			case 4:
+				System.out.println("El nombre del beneficiario es " +
+			cuenta_personal.beneficiario);
+				break;
+			case 5:
+				System.out.println("Ingrese un nuevo beneficiario");
+				String nombre = leer.next();
+				cuenta_personal.beneficiario = nombre;
+				break;
+		}
+			
+			
+		} while(opcion != 6); 
+			
+		
+			
+		
+	}
+}
+		
+		
+		/*
+		cuenta_personal.beneficiario = "Miguel Alexis";
+		System.out.println("Ingresa un monto a depositar");
+		float deposito = leer.nextFloat();
+		
+		System.out.println("Ingresa un monto a retirar");
+		float retiro = leer.nextFloat();
+		
+		cuenta_personal.depositos(deposito);
+		cuenta_personal.retiros(retiro);
+		System.out.println("Tu saldo actual es " + cuenta_personal.consultarSaldo());
+		
+		System.out.println(cuenta_personal.beneficiario);
+		
+		CuentaBancaria cuenta_papa = new CuentaBancaria();
+		cuenta_papa.beneficiario = "Miguel";
+		
+		System.out.println(cuenta_papa.beneficiario);*/
+	
+	
+	
